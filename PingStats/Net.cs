1686841642bd81162c78dd71f3e259bc54f61624
Blankpanda <Blankpanda8@gmail.com>
2015-09-28 12:23:22 -0400

    input checking for IP addresses

---
+++ PingStats/Net.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+namespace PingStats
+{
+	class Net
+	{
+		// checks if the inputed string is an IP address
+		public static bool IsIPAddress(string ipString)
+		{
+			string[] octs = ipString.Split('.'); // checks to see if the vlaues are in the right format
+			if (octs.Length != 4)
+			{
+				return false;
+			}
+
+			byte b = 0; // checks each value in octs to make sure its a valid byte
+			for (int i = 0; i < octs.Length; i++)
+			{
+				if (!byte.TryParse(octs[i], out b))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		// checks if the inputed string is a domain name
+
+		public static void IsDomainName(string dnString)
+		{
+
+			IPHostEntry x = Dns.Resolve(dnString);
+			Console.WriteLine(x.AddressList[0]);
+
+		}
+	}
+}
+
--- PingStats/Program.cs
+++ PingStats/Program.cs
@@ -6,45 +6,80 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 namespace PingStats
 {
-    class Program
-    {
-        static void Main(string[] args)
-       {
-            Pinger p = new Pinger(); // object of Pinger.cs to get replies and ping addressess
-            List<long> reps = new List<long>(); // list of replies
+	class Program
+	{
+		static void Main(string[] args)
+	   {
+			Pinger p = new Pinger(); // object of Pinger.cs to get replies and ping addressess
+			List<long> reps = new List<long>(); // list of replies
 
-            // Input
+			Net.IsDomainName("google.com");
+			// user entry
+			string inAddress = "";
+			string inPingAmount = "";
 
-            Console.WriteLine("Enter an IP address or a hostname: ");
-            string Address = 
-            Console.ReadLine();
+			while (true) // we need to make sure that the user enters in a string or an IP address
+			{
+				Console.WriteLine("Enter an IP address or a hostname: ");
 
-            Console.WriteLine("Enter in the number of times you want to ping this address");
-            string inPingAmount =
-            Console.ReadLine();
+				try // if the address doesnt throw an error we can check to see if its a valid IP / hostname
+				{	        
+					inAddress =
+					Console.ReadLine();
 
-            // Cast PingAmount to a long
-            long PingAmount = long.Parse(inPingAmount);
-            
-            while (reps.Count <= PingAmount)
-            {
-                p.PingAdd(Address); // send a ICMP message
-                reps = p.Replies;  // populate the list with the replies                                                                                
-            }
+					// checks if the input is an IP address/domain name, if not then we continue the loop.
+					if (Net.IsIPAddress(inAddress))
+					{
+						break; // true, the string is an IP address
+					}
+					//else if (Net.IsDomainName(inAddress))
+					//{
+					//    break; // true, the string is a domain name
+					//}
+					else
+					{
+						Console.WriteLine("Invalid address, use x.x.x.x format for IP or a domain name");
+						inAddress = "";
+						continue; // false, redo the loop.
+					}
+				}
+				catch (Exception)
+				{
+					// the output is invalid.
+					Console.WriteLine("Invalid address, use x.x.x.x format for IP or a domain name");
+					inAddress = "";
+					continue;				            
+				}
+				
+			}
+		  
 
-            // Use Json.net to convert the List into a .json file.
-            string json = JsonConvert.SerializeObject(reps.ToArray(), Formatting.Indented);
+			Console.WriteLine("Enter in the number of times you want to ping this address");
+			 inPingAmount =
+			Console.ReadLine();
 
-            // write the formatted JSON to a created file.
-            FileWriter writer = new FileWriter();
+			// Cast PingAmount to a long
+			long PingAmount = long.Parse(inPingAmount);
+			
+			while (reps.Count <= PingAmount)
+			{
+				p.PingAdd(inAddress); // send a ICMP message
+				reps = p.Replies;  // populate the list with the replies                                                                                
+			}
 
-            string writeFile = writer.CreateFileName();
-            writer.WriteToFile(writeFile, json);
+			// Use Json.net to convert the List into a .json file.
+			string json = JsonConvert.SerializeObject(reps.ToArray(), Formatting.Indented);
 
-            // change the file to JSON format
-            writer.ChangeFileExten(writeFile,".json");
+			// write the formatted JSON to a created file.
+			FileWriter writer = new FileWriter();
 
-            Console.ReadLine();
-        }
-    }
+			string writeFile = writer.CreateFileName();
+			writer.WriteToFile(writeFile, json);
+
+			// change the file to JSON format
+			writer.ChangeFileExten(writeFile,".json");
+
+			Console.ReadLine();
+	   }
+	}
 }
