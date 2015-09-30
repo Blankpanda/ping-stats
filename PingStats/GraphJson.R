34408620fa29e231af922bf74b3f963a95a657e5
Blankpanda <Blankpanda8@gmail.com>
2015-09-30 13:56:17 -0400

    worked on formatting the graph

--- PingStats/GraphJson.R
+++ PingStats/GraphJson.R
@@ -1,12 +1,17 @@
 library(jsonlite)
 library(ggplot2)
 pingStats <- fromJSON("bin/Debug/ubuntu.com_0.json")
+statusStats <- fromJSON("bin/Debug/status_ubuntu.com_0.json")
 
 stats <- data.frame(pingStats) # convert the data to a data.frame
-stats.index <- data.frame(1:length(pingStats)) # new row to bind
+status <- data.frame(statusStats)
+
+stats.index <- data.frame(1:length(pingStats)) # new row to bind to stats
 
 stats.Combined <- cbind(stats,stats.index)
-names(stats.Combined) <- c("ms", "index")
+stats.Combined <- cbind(stats.Combined,status)
+
+names(stats.Combined) <- c("ms", "index", "status")
 
 plot <- ggplot(stats.Combined, aes(x = stats.Combined$index ,
                                    xmin = stats.Combined$index,
@@ -14,6 +19,6 @@ plot <- ggplot(stats.Combined, aes(x = stats.Combined$index ,
                                    ymin = stats.Combined$ms + 75
                                    )) +
         geom_area(size = 1 , alpha = 0.1) +
-        geom_point() +
+        geom_point( ) +
         geom_line() 
 plot
