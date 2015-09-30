b7ca0380762275ac84c6dfad65075d5e190a3d39
Blankpanda <Blankpanda8@gmail.com>
2015-09-30 11:54:24 -0400

    worked on the graph itself, need to find a way to retrieve if the ping was a success or not and use it color code the graph

--- PingStats/GraphJson.R
+++ PingStats/GraphJson.R
@@ -1,6 +1,6 @@
 library(jsonlite)
 library(ggplot2)
-pingStats <- fromJSON("bin/Debug/pixiv.com_1.json")
+pingStats <- fromJSON("bin/Debug/linuxforcynics.com_2.json")
 
 stats <- data.frame(pingStats) # convert the data to a data.frame
 stats.index <- data.frame(1:length(pingStats)) # new row to bind
@@ -8,7 +8,11 @@ stats.index <- data.frame(1:length(pingStats)) # new row to bind
 stats.Combined <- cbind(stats,stats.index)
 names(stats.Combined) <- c("ms", "index")
 
-plot <- ggplot(stats.Combined, aes(x = stats.Combined$index , y = stats.Combined$ms)) +
-        geom_area( position = "stack",size = 1, color= "Grey") + 
-        geom_point(color="red")
+plot <- ggplot(stats.Combined, aes(x = stats.Combined$index ,
+                                   xmin = stats.Combined$index,
+                                   y = stats.Combined$ms ,
+                                   ymin = stats.Combined$ms + 25
+                                   )) +
+        geom_area(size = 1 , alpha = 0.1) +
+        geom_point()
 plot
