28b30fe1675f44843b9db53660bfd5e12a4e9d61
Blankpanda <Blankpanda8@gmail.com>
2015-10-01 14:06:25 -0400

    moved files around

--- PingStats/Program.cs
+++ PingStats/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -134,6 +135,10 @@ namespace PingStats
 
 			writer.WriteToFile(ResultFileName, resultData);
 			writer.ChangeFileExten(ResultFileName, ".json");
+
+			// run a R script with params
+			RunRScript.RunFromCmd("GraphJson.R",)
+			
 
 			Console.ReadLine();
 	   }
---
+++ PingStats/source/RunRScript.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingStats
+{
+    class RunRScript
+    {
+
+        public static string RunFromCmd(string ScriptPath, string ScriptExecPath)
+        {
+            string file = ScriptPath;
+            string result = string.Empty;
+
+            try
+            {
+                var info = new ProcessStartInfo();
+                info.FileName = ScriptExecPath;
+                info.WorkingDirectory = Path.GetDirectoryName(ScriptExecPath);
+                info.Arguments = ScriptPath + " ";
+
+                info.RedirectStandardInput = false;
+                info.RedirectStandardOutput = true;
+                info.UseShellExecute = false;
+                info.CreateNoWindow = true;
+
+                using (var proc = new Process())
+                {
+                    proc.StartInfo = info;
+                    proc.Start();
+                    result = proc.StandardOutput.ReadToEnd();
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+    }
+}
